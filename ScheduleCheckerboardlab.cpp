e4ff35bd8e8a4b9027252c9edc1db462e31af3c4
RobertArnold1 <89536170+RobertArnold1@users.noreply.github.com>
2021-09-16 20:18:53 -0500

    Add project files.

---
+++ ScheduleCheckerboardlab.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Tuesday 8:00 Python \n";
+	cout << "Tuesday 10:00 C++\n";
+
+	for (int i = 1; i <= 4; ++i)
+	{
+		cout << "*   *   *   *\n";
+		cout << "  *   *   *   *\n";
+	}
+}
